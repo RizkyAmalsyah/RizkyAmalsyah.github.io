89df03c3a8d3f67edc7dd0a5cd90eb2b7241db31
Muhammad Rizky Amalsyah <108131203+RizkyAmalsyah@users.noreply.github.com>
2023-04-30 14:09:28 +0700

    upload project

---
+++ public/js/interactive.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+    $("#klik").click(function() {
+        $("#content").show(1800);
+        $("#klik").hide(1000);
+    });
+
+  var jam = new Date().getHours();
+  var pukul = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();;
+	var pesan;
+	var pagi = ('Selamat Pagiii');
+	var siang = ('Selamat Sianggg');
+	var sore = ('Selamat Soreee');
+	var malam = ('Selamat Malammm');
+
+	if (jam >= 6 && jam < 10) {
+		pesan = pagi; 
+	} else if (jam >= 10 && jam < 15) {
+		pesan = siang;
+  } else if (jam >= 15 && jam < 18) {
+		pesan = sore;
+	} else if (jam >= 18 && jam < 6) {
+		pesan = malam;
+	}
+	$('#waktu').append(pesan);
+  $('#pukul').append(pukul);
+  
+});
+
